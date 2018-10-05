d0250da2dc179e010fefdce0ef826b9780339b3b
Minami Munakata <june28munazo@gmail.com>
2018-10-04 19:03:02 -0700

    answer for 1.1

--- src/Ch01ArraysAndStrings/Q1_01_Is_Unique.java
+++ src/Ch01ArraysAndStrings/Q1_01_Is_Unique.java
@@ -1,9 +1,36 @@
+/*
+ *
+ * Created by minami on 2018-10-03.
+ * Copyright © 2018 Minami Munakata. All rights reserved.
+ *
+ */
+
 package Ch01ArraysAndStrings;
 
+
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Q1_01_Is_Unique {
+    
+//    TODO: Only allowed to use either ARRAY or STRING
+
+    public static boolean isUniqueChars(String word) {
+        Set<Character> characterSet = new HashSet<>();
+        for (int i = 0; i < word.length(); i++) {
+            characterSet.add(word.charAt(i));
+            if (characterSet.size() <= i) return false;
+        }
+        return true;
+    }
+
 
     public static void main(String[] args) {
-        
+        String[] words = {"abcde", "hello", "apple", "kite", "padle"};
+        for (String word : words) {
+            System.out.println(word + ": " + isUniqueChars(word));
+        }
     }
 
 
